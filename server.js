998e228afedaeb83f7fd6820717b1c9c71a4b694
Glitch (iodized-hot-idea) <none>
2020-07-28 11:49:16 +0000

    ✂️🛍 Checkpoint ./server.js:18828285/138

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const session = require('express-session')
 app.set('view engine', 'pug'); //Sets pug as template engine
 fccTesting(app); //For FCC testing purposes
 
+//---- accessing files on public and parsing bodies of requests
 
 app.use("/public", express.static(process.cwd() + "/public"));
 app.use(express.json());
@@ -26,7 +27,7 @@ app.use(session({
 //----- using Passport to manage logins credentials -----
 
 app.use(passport.initialize());
-
+app.use(passport.session());
 
 
 app.route("/").get((req, res) => {  //rendering of templates
